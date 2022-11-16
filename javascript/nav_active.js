44633b84511c43c4ae160ca10328dc0790169f79
TimofeyTst <yarik.mist@gmail.com>
2022-11-17 00:28:55 +0300

    Stability without footer

--- javascript/nav_active.js
+++ javascript/nav_active.js
@@ -1,21 +1,18 @@
-
+document.addEventListener("DOMContentLoaded", () =>{
 const burger = document.querySelector('.burger');
 const nav = document.querySelector('.nav-links');
 const nav_links = document.querySelectorAll('.nav-link');
 
-function nav_active() {
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        burger.classList.toggle('burger-active');
-    
-        nav_links.forEach((link, index) => {
-            if(link.style.animation){
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinksAnim .5s ease forwards ${index/7 + 0.3}s`;
-            }
-        })
-    });
-}
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('burger-active');
 
-nav_active();
+    nav_links.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinksAnim .5s ease forwards ${index/7 + 0.3}s`;
+        }
+    })
+});
+})
---
+++ javascript/small_devices.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () =>{ 
+    if (window.matchMedia("(max-width: 600px)").matches) {
+        let images = document.querySelectorAll("div.container .cards .card .content img");
+        images.forEach(image=>{
+            image.parentNode.classList = "header";
+            image.parentNode.innerHTML = "<p>Header</p><a class=\"button\" href=\"#\"><img src=\"src/close.png\"></a>";
+        });
+
+        let buttons = document.querySelectorAll("div.container .cards .card .header a");
+        buttons.forEach(button => {
+            button.addEventListener("click", openCard.bind(button));
+        });
+    }
+})
+
+
+function openCard(button) {
+    button.path[3].classList.toggle('open');
+    if (button.path[0].src == "http://127.0.0.1:5501/src/open.png"){
+        button.path[0].src = "src/close.png";
+    } else {
+        button.path[0].src = "src/open.png";
+    }
+}
